61e2a7489d33be1ee148cdaa4664e3a3cce71498
lesley okoduwa <45374070+oselesley@users.noreply.github.com>
2021-01-14 19:03:25 +0100

    edit aspect

--- src/main/java/com/internship/walletapi/aop/logging/LoggingAspect.java
+++ src/main/java/com/internship/walletapi/aop/logging/LoggingAspect.java
@@ -1,24 +1,23 @@
 package com.internship.walletapi.aop.logging;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
+
+
+import java.util.Arrays;
 
 @Aspect
 public class LoggingAspect {
-    private final Environment environment;
+    @Autowired
+    private Environment env;
 
-    private Logger logger = LogManager.getLogger(LoggingAspect.class);
-
-    public LoggingAspect(Environment environment) {
-        this.environment = environment;
+    private Logger logger(JoinPoint joinPoint) {
+        return LoggerFactory.getLogger(joinPoint.getSignature().getDeclaringTypeName());
     }
 
     @Pointcut("within(@org.springframework.stereotype.Repository *)" +
@@ -35,28 +34,48 @@ public class LoggingAspect {
     }
 
     @Before(value = "springBeanPointcut() || applicationPointCut()")
-    public void logger(JoinPoint joinPoint) {
-       Object[] args = joinPoint.getArgs();
-       StringBuilder argsBuilder = new StringBuilder();
-
-
-        for (int index = 0; index < args.length; index++) {
-            argsBuilder.append(args[index].toString());
-        }
-
-       logger.info("Entering method: with arguments {} {}: " + argsBuilder.toString());
+    public void loggerMethods(JoinPoint joinPoint) {
+        Logger logger = logger(joinPoint);
+       logger.info("Entering method: with arguments {} {}: " + Arrays.toString(joinPoint.getArgs()));
     }
 
     @Around(value = "springBeanPointcut() || applicationPointCut()")
-    public void logger(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        logger.info("Proceeding to enter method: ");
-        proceedingJoinPoint.proceed();
-        logger.info("Finished execution, leaving method: ");
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        Logger log = logger(joinPoint);
+        if (log.isDebugEnabled()) {
+            log.debug("Enter: {}() with argument[s] = {}", joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+        }
+        try {
+            Object result = joinPoint.proceed();
+            if (log.isDebugEnabled()) {
+                log.debug("Exit: {}() with result = {}", joinPoint.getSignature().getName(), result);
+            }
+            return result;
+        } catch (IllegalArgumentException e) {
+            log.error("Illegal argument: {} in {}()", Arrays.toString(joinPoint.getArgs()), joinPoint.getSignature().getName());
+            throw e;
+        }
     }
 
     @AfterThrowing(value = "applicationPointCut() || springBeanPointcut()", throwing = "e")
-    public void logExceptions(Exception e) {
-        logger.info(e.getMessage());
-        e.printStackTrace();
+    public void logExceptions(JoinPoint joinPoint, Throwable e) {
+
+        if (env.getActiveProfiles().length > 0) {
+            logger(joinPoint)
+                    .error(
+                            "Exception in {}() with cause = \'{}\' and exception = \'{}\'",
+                            joinPoint.getSignature().getName(),
+                            e.getCause() != null ? e.getCause() : "NULL",
+                            e.getMessage(),
+                            e
+                    );
+        } else {
+            logger(joinPoint)
+                    .error(
+                            "Exception in {}() with cause = {}",
+                            joinPoint.getSignature().getName(),
+                            e.getCause() != null ? e.getCause() : "NULL"
+                    );
+        }
     }
 }
